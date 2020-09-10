820167dcb49718ae8cd561affe0b823452fe7651
Redika West <53380131+redikawest@users.noreply.github.com>
2020-09-10 17:00:46 +0700

    Update App.js

--- CobaReact/App.js
+++ CobaReact/App.js
@@ -11,6 +11,7 @@ function App() {
       <StatusBar barStyle="dark-content" backgroundColor="transparent"  />
       <ImageBackground style={{width: win.width, height: win.height, position: "absolute",flexDirection:"column", alignItems:"center", justifyContent:"center"}} source={gambar}>
         <Text style={{backgroundColor:"red", paddingHorizontal: 16, paddingVertical:4, borderRadius: 10, color: "white"}} >Hello World</Text>
+        <Text style={{backgroundColor:"red", paddingHorizontal: 16, paddingVertical:4, borderRadius: 10, color: "white", marginTop:10}} >hay kamu</Text>
       </ImageBackground>
     </View>
     // </NavigationContainer>
